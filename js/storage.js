618432f6def9d18eee8fbe527c1b9abd9a5c5dc1
wangdian19830925 <wangdian19830925@hotmail.com>
2026-06-29 19:25:25 +0800

    v175: 替换蔚来和九号公司官方 logo

--- js/storage.js
+++ js/storage.js
@@ -459,7 +459,7 @@ const Storage = {
     return {
       data: data,
       updatedAt: new Date().toISOString(),
-      clientVersion: 'v174',
+      clientVersion: 'v175',
       passwordHash: pwdHash,
       passwordEnabled: pwdEnabled
     };
